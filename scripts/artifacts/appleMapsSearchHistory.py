74fd84fbaaa495d174b0e2d93959c025f7616290
Sourcery AI <>
2023-07-30 04:09:30 +0000

    'Refactored by Sourcery'

--- scripts/artifacts/appleMapsSearchHistory.py
+++ scripts/artifacts/appleMapsSearchHistory.py
@@ -20,7 +20,7 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
     data_list = []
 
     shorttypes = {'1': {'type': 'message', 'message_typedef': {'1': {'type': 'bytes', 'name': ''}}, 'name': ''}, '7': {'type': 'int', 'name': ''}, '8': {'type': 'message', 'message_typedef': {'4': {'type': 'message', 'message_typedef': {'3': {'type': 'int', 'name': ''}, '4': {'type': 'message', 'message_typedef': {'1': {'type': 'message', 'message_typedef': {'1': {'type': 'double', 'name': ''}, '2': {'type': 'double', 'name': ''}}, 'name': ''}}, 'name': ''}}, 'name': ''}}, 'name': ''}}
-    
+
     longtypes = {'1': {'type': 'message', 'message_typedef': {'1': {'type': 'bytes', 'name': ''}}, 'name': ''}, '7': {'type': 'int', 'name': ''}, '8': {'type': 'message', 'message_typedef': {'1': {'type': 'message', 'message_typedef': {'3': {'type': 'bytes', 'name': ''}, '4': {'type': 'message', 'message_typedef': {'1': {'type': 'message', 'message_typedef': {'5': {'type': 'double', 'name': ''}, '6': {'type': 'double', 'name': ''}, '7': {'type': 'double', 'name': ''}, '8': {'type': 'double', 'name': ''}}, 'name': ''}, '3': {'type': 'int', 'name': ''}}, 'name': ''}, '7': {'type': 'message', 'message_typedef': {'1': {'type': 'message', 'message_typedef': {'1': {'type': 'bytes', 'name': ''}, '2': {'type': 'message', 'message_typedef': {'1': {'type': 'int', 'name': ''}, '2': {'type': 'int', 'name': ''}}, 'name': ''}}, 'name': ''}, '2': {'type': 'message', 'message_typedef': {'1': {'type': 'bytes', 'name': ''}}, 'name': ''}, '3': {'type': 'int', 'name': ''}, '5': {'type': 'message', 'message_typedef': {'1': {'type': 'message', 'message_typedef': {'1': {'type': 'double', 'name': ''}, '2': {'type': 'double', 'name': ''}}, 'name': ''}, '2': {'type': 'int', 'name': ''}, '3': {'type': 'int', 'name': ''}}, 'name': ''}, '12': {'type': 'message', 'message_typedef': {'1': {'type': 'bytes', 'name': ''}}, 'name': ''}, '15': {'type': 'int', 'name': ''}, '16': {'type': 'message', 'message_typedef': {'1': {'type': 'fixed32', 'name': ''}}, 'name': ''}, '10000': {'type': 'bytes', 'name': ''}, '10001': {'type': 'int', 'name': ''}, '10011': {'type': 'message', 'message_typedef': {'1': {'type': 'double', 'name': ''}, '2': {'type': 'double', 'name': ''}, '6': {'type': 'bytes', 'name': ''}}, 'name': ''}, '10019': {'type': 'int', 'name': ''}}, 'name': ''}}, 'name': ''}}, 'name': ''}}
 
 
@@ -28,17 +28,15 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
 
     for file_found in files_found:
         file_found = str(file_found)
-        
+
         with open(file_found, "rb") as plist_file:
             plist_content = plistlib.load(plist_file)
             #print(plist_content)
-            
+
             for a, b in plist_content.items():
                 pass
-                
+
             for c, d in b.items():
-                if isinstance(d, str):
-                    pass
                 if isinstance(d, dict): #records
                     for e, f in d.items():
                         guid = e
@@ -63,12 +61,12 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                         #yelp like stuff
                                         pp = pprint.PrettyPrinter(indent = 0)
                                         items = pp.pformat(protostuff.get('7')['1']['1'][1])
-                                        
-                                                
+
+
                                 if protostuff.get('6'):
                                     try:
                                         location = protostuff['6']['1'].decode()
-                                        location = location + ', ' + protostuff['6']['2'].decode() #GPs after it?
+                                        location = f'{location}, ' + protostuff['6']['2'].decode()
                                     except:
                                         pass
                                 try:    
@@ -76,7 +74,6 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                         if (protostuff['8']['2']['1'].get('201')):
                                             usersearchnotinproto = (protostuff['8']['2']['1']['201']['2']['2']['1'].decode())
                                 except:
-                                    pass
                                     try:
                                         for mira in (protostuff['8']['2']['1']['4']):
                                             #print(mira)
@@ -96,23 +93,23 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                                         geo6 = longstuff['8']['1']['7']['5']['1']['2']
                                                     except:
                                                         pass
-                                                        
+
                                                     try: 
-                                                        
+
                                                         shortstuff = shortbase64proto(mira['11']['2'].decode(), shorttypes)
-                                                        
+
                                                         shortlat = (shortstuff['8']['4']['4']['1']['1'])
                                                         shortlon = (shortstuff['8']['4']['4']['1']['2'])
-                                                        
-                                                        
+
+
                                                     except:
                                                         pass
-                                                    
+
                                                     if mira.get('8'):
                                                         pname = (mira.get('8')['1']['10']['3'].decode())
                                     except:
                                         currentlocation = (protostuff['8']['2']['8']['3'].decode())
-                                        
+
                                 try: 
                                     if protostuff.get('7'):
                                         shortstuff = shortbase64proto(protostuff.get('7')['1']['1'][0]['2']['1']['4'][8]['11']['2'].decode(), shorttypes)
@@ -120,9 +117,9 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                         location = (shortstuff['8']['1']['3'].decode())
                                         shortlat = (shortstuff['8']['4']['4']['1']['1'])
                                         shortlon = (shortstuff['8']['4']['4']['1']['2'])
-                                        
+
                                         if (protostuff.get('7')['1']['1'][1]['1'].get('2')):
-                                            
+
                                             #pp.pprint(protostuff.get('7')['1']['1'][1]['1'].get('2')['4']) #address
                                             pname = (protostuff.get('7')['1']['1'][1]['1'].get('2')['5'].decode()) #place name
                                             shortadd = ''
@@ -130,9 +127,6 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                                 shortadd = shortadd  + parts.decode() + ', '
                                             shortadd = (shortadd[:-2])
                                 except:
-                                    pass
-                                    
-                                    
                                     """
                                     counter = 0
                                     for masa in protostuff.get('7')['1']['1'][1]['2']['1']['4']:
@@ -162,13 +156,13 @@ def get_appleMapsSearchHistory(files_found, report_folder, seeker, wrap_text):
                                     except:
                                         pp = pprint.PrettyPrinter(indent = 0)
                                         items = pp.pformat(protostuff['7']['1']['1'][0]['2']['1']['4'])
-                                        
+
                                 data_list.append((modificationdate,app,location,shortadd,pname,shortlat,shortlon,usersearchnotinproto,usersearch1, usersearch2,geo1,geo2,geo3,geo4,geo5,geo6,guid,currentlocation,items ))
                                 modificationdate = app = locatio = shortadd = pname = shortlat = shortlon = usersearch1 = usersearch2 = geo1 = geo2 = geo3 = geo4 = geo5 = geo6 = guid = items = currentlocation = '' 
-        
-        
 
-    if len(data_list) > 0:
+
+
+    if data_list:
         report = ArtifactHtmlReport('Apple Maps Search History')
         report.start_artifact_report(report_folder, 'Apple Maps Search History')
         report.add_script()
